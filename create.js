af6131cb4a87e538ed1e2948931bf6763dec96e4
Mochi McCoy <hi@mochibun.me>
2023-09-12 15:22:13 +0200

    Input fields auto resize

--- create.js
+++
@@ -1,9 +0,0 @@
-console.log(`Hello from add.js!`);
-
-function resizeTextarea() {
-  const textarea = document.getElementById('my-textarea');
-  textarea.style.height = 'auto';
-  textarea.style.height = `${textarea.scrollHeight}px`;
-}
-const textarea = document.getElementsByClassName('listItem');
-textarea.addEventListener('input', resizeTextarea);
