5529611ca7fb1acf811b549cc5e43e3cb33a6b5d
Mochi McCoy <hi@mochibun.me>
2023-09-13 09:09:56 +0200

    Added dynamic tab title, removed deprecated code

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-console.log(`Hello from index.js!`);
+console.log(`Hello from index.js!`); // test
 
-// const tabTitle = document.getElementById(`listTitleInput`).input;
-// tabTitle.addEventListener("keydown", ((e) => {
-//   document.title = e;
-// };
+const listTitle = document.querySelector("#listTitleInput");
+listTitle.addEventListener("keyup", (() => {
+  console.log(listTitle.value);
+  listTitle.value !== `` ? document.title = listTitle.value + ` | To-Do Beast List` : document.title = `Untitled | To-Do Beast List`;
+})); // * Function to change the tab title with the To-Do list title
 
-document.ready(function() {
-  input.keyup(function() {
-      let text = this.value;
-      document.title = text;
-  });
-});
