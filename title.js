f68add600aee54e66d94ce7681c3696dd82b8c7f
Mochi McCoy <hi@mochibun.me>
2023-09-16 23:10:23 +0200

    Minor code changes, WIP

--- title.js
+++ title.js
@@ -3,23 +3,23 @@ const listTitle = document.querySelector("#listTitleInput");
 
 
 window.addEventListener("load", (() => {
-  if (localStorage.getItem(`title`) === null){ //*If key does not exist
-    localStorage.setItem(`title`, `Untitled`); //* Create new key with default text
-    listTitle.placeholder = `Untitled`; //* Set matching value to title
-    document.title = listTitle.placeholder + ` | To-Do Beast List`; //* Set matching value to tab title
-    return; //* Stop executing the rest
-  }; //* Should the if condition fail, run this 
-  const titleStore = localStorage.getItem(`title`); //* Get title content
+  if (localStorage.getItem(`title`) === null){ //If key does not exist
+    localStorage.setItem(`title`, `Untitled`); // Create new key with default text
+    listTitle.placeholder = `Untitled`; // Set matching value to title
+    document.title = listTitle.placeholder + ` | To-Do Beast List`; // Set matching value to tab title
+    return; // Stop executing the rest
+  }; // Should the if condition fail, run this 
+  const titleStore = localStorage.getItem(`title`); // Get title content
   // console.log(`Here is the tab title!`); //! DEBUG
   // console.log(titleStore);  //! DEBUG
-  if (titleStore === `Untitled`){ //* If the title is `Untitled`, set it as a placeholder
+  if (titleStore === `Untitled`){ // If the title is `Untitled`, set it as a placeholder
     listTitle.placeholder = `Untitled`;
     document.title = `Untitled | To-Do Beast List`;
-  } else { //* Otherwise, set the title as a value
-    listTitle.value = titleStore; //* Set the list title to the key value
+  } else { // Otherwise, set the title as a value
+    listTitle.value = titleStore; // Set the list title to the key value
     document.title = titleStore + ` | To-Do Beast List`; //* Set tab title
   }
-}));//* Function that checks, retrieves and sets the list title as well as the tab title
+}));// Function that checks, retrieves and sets the list title as well as the tab title
 
 listTitle.addEventListener("keyup", (() => {
   if (listTitle.value !== ``){
@@ -29,14 +29,14 @@ listTitle.addEventListener("keyup", (() => {
   } else {
   document.title = `Untitled | To-Do Beast List`;
   };
-})); //* Function to change the tab title with the To-Do list title
+})); // Function to change the tab title with the To-Do list title
 
 listTitle.addEventListener("blur", (() => {
-  if (listTitle.value === ``){ //* If the user clears the title, replace it with a placeholder that says `Untitled`
+  if (listTitle.value === ``){ // If the user clears the title, replace it with a placeholder that says `Untitled`
     listTitle.placeholder = `Untitled`;
-    localStorage.setItem(`title`, `Untitled`); //* "Reset" the title key to the "default" value
-    return; //* Stop executing the rest
-  } //* Otherwise, trim the user's input and replace the user's input
+    localStorage.setItem(`title`, `Untitled`); // "Reset" the title key to the "default" value
+    return; // Stop executing the rest
+  } // Otherwise, trim the user's input and replace the user's input
   const titleTrim = listTitle.value.trim();    
   listTitle.value = titleTrim;
-})); //* Function to trim the input field from any whitespace but only when the user switches to a different object. Doing it in the previous function would force trim the input, preventing any easy text separation
+})); // Function to trim the input field from any whitespace but only when the user switches to a different object. Doing it in the previous function would force trim the input, preventing any easy text separation
--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 console.log(`Hello from update.js! ENGINE REWORK IN PROGRESS`); //!Debug
-import { addTodo, list, todoItems, txtTrim } from "./create1.js"; //Import data array and text trimming functions
+import { addTodo, list, setTodoItems, todoItems, txtTrim } from "./create1.js"; //Import data array and text trimming functions
 import { deleteTodo } from "./delete.js";
 import { txtResize } from "./resize.js"; //Import a function that automatically resizes the text input area to fit more text
 
@@ -52,21 +52,32 @@ export const renderTodo = (todo, theId) => { //start of the function
   node.appendChild(newBtn); //Append the new button to the LI
 
 
-  const position = list.querySelector(`[data-key='${todo.id}']`);
-  console.log(`Here is the list:`);
-  console.log(list);
-  console.log(`Here's the data key`);
-  console.log(todo.id);
-  console.log(position);
+  // const position = list.querySelector(`[data-key='${todo.id}']`);
+  // console.log(`Here is the list:`);
+  // console.log(list);
+  // console.log(`Here's the data key`);
+  // console.log(todo.id);
+  // console.log(position);
+  // TODO See if you can figure out how to add item at current position
+
 
   list.appendChild(node); //Append the new LI to the UL
   newTextArea.focus() //Focus input on the newly created textbox
+  txtTrim(newTextArea); //Add an event listener that trims user input
 
-  document.addEventListener("blur", () => {
-    updateFunc;
+  newTextArea.addEventListener("keyup", () => {
+    const itemLi = document.querySelector(`[data-key='${todo.id}']`);
+    const txtArea = itemLi.querySelector(`textarea`);
+    const text = txtArea.value;
+    const updateObj = {
+      text,
+      checked: todo.checked,
+      id: todo.id,
+    };
+    // TODO Add a function to splice the array and replace the object data with new object data
+    //? Perhaps the array.splice() method might be the answer
   });
   
-  txtTrim(newTextArea); //Add an event listener that trims user input
   txtResize(newTextArea); //Add an event listener that automatically resizes the textbox vertically depending on the amount of text
   
   if (item) {
@@ -75,11 +86,11 @@ export const renderTodo = (todo, theId) => { //start of the function
     list.append(node);
   }
 };
-const updateFunc = () => {
-  const item = document.querySelector(`[data-key='${todo.id}']`);
 
-};
 
+
+// const updateFunc = () => {
+// };
 
   // newTextArea.classList.add(`listItem`);
   // newTextArea.setAttribute(`id`, todo.id);
